aa1662726bf925f51b037153a2f2b5be8843aeb4
Ivan Baluta <ltlaitoff@gmail.com>
2022-02-14 15:59:54 +0200

    init commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Header from 'components/Header/Header.jsx'
+
+import styles from './App.module.scss'
+
+function App() {
+	return (
+		<div className={styles.app}>
+			<Header />
+		</div>
+	)
+}
+
+export default App
