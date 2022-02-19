8c88871f279695d59881d9ea084c106c4aeb1627
Ivan Baluta <ltlaitoff@gmail.com>
2022-02-19 15:52:03 +0200

    Created header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,40 @@
 import React from 'react'
-import Logo from 'components/Logo/Logo.jsx'
-
+import { Link } from 'react-router-dom'
+import Logo from 'components/Logo'
+import Menu from 'components/Menu/Menu.jsx'
 import styles from './Header.module.scss'
+import Languages from 'components/Languages'
+import { useSelector } from 'react-redux'
+
+const logoText = {
+	ru: 'здоровый рацион',
+	ua: 'здоровий раціон',
+	en: 'healthy ration'
+}
 
 const Header = () => {
+	const currentLanguage = useSelector(store => store.language.language)
+
 	return (
-		<div className={styles.header}>
-			<Logo />
-			<Logo type='big' />
-		</div>
+		<header className={styles.header}>
+			<div className={styles.headerFistPart}>
+				<Link to='/' className={styles.logo}>
+					<Logo link={false} />
+					<div className={styles.logoText}>{logoText[currentLanguage]}</div>
+				</Link>
+
+				<div className={styles.wrapper}>
+					<Menu className={styles.menu} />
+					<Languages className={styles.languages} />
+				</div>
+			</div>
+
+			<div className={styles.headerSecondPart}>
+				<a href='tel:000' className={styles.phone}>
+					+38 (068) 949 - 49 -19
+				</a>
+			</div>
+		</header>
 	)
 }
 
