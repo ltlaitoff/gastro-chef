8f215a3268657d42ae9cf112cb0baa9b81f22388
Ivan Baluta <ltlaitoff@gmail.com>
2022-03-08 01:41:21 +0200

    Create main structure and create content half

--- src/components/MainPromo/MainPromo.jsx
+++ src/components/MainPromo/MainPromo.jsx
@@ -1,69 +1,24 @@
 /* eslint-disable */
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import ReactMarkdown from 'react-markdown'
 
 import styles from './MainPromo.module.scss'
 import DATA from './data.js'
-import { useSelector } from 'react-redux'
 import MenuAddition from 'components/MenuAddition'
-
-const transformText = text => {
-	const components = {
-		em: ({ node, ...props }) => <span className={styles.green} {...props} />,
-
-		strong: ({ node, ...props }) => (
-			<span className={styles.greenBig} {...props} />
-		),
-		p: React.Fragment
-	}
-
-	return <ReactMarkdown children={text} components={components} />
-}
-
-const getDataFromCurrentSlide = (currentSlide, currentLanguage) => {
-	return {
-		id: currentSlide.id,
-		promoText: transformText(currentSlide.promoText[currentLanguage]),
-		promoPostText: transformText(currentSlide.promoPostText[currentLanguage]),
-		buttonText: transformText(currentSlide.buttonText[currentLanguage]),
-		buttonPostText: transformText(currentSlide.buttonPostText[currentLanguage]),
-		image: currentSlide.image,
-		url: currentSlide.url
-	}
-}
+import MainPromoContent from './components/MainPromoContent'
 
 const MainPromo = () => {
 	const [currentSlideId, setCurrentSlideId] = useState(0)
-	const currentLanguage = useSelector(store => store.language.language)
 
 	const currentSlide = DATA.filter(slide => slide.id === currentSlideId)[0]
 
-	const {
-		id,
-		promoText: contentTitleText,
-		promoPostText: contentPostTitleText,
-		buttonText,
-		buttonPostText: postButtonText,
-		imageSrc,
-		url
-	} = getDataFromCurrentSlide(currentSlide, currentLanguage)
-
 	return (
-		<section className={styles.promo} key={id}>
+		<section className={styles.promo}>
 			<div className={styles.wrapper}>
 				<MenuAddition />
-				<div className={styles.content}>
-					<h2 className={styles.contentTitle}>{contentTitleText}</h2>
-					<div className={styles.contentPostTitle}>{contentPostTitleText}</div>
-					<Link to={url} className={styles.button}>
-						{buttonText}
-					</Link>
-					<div className={styles.postButton}>{postButtonText}</div>
-				</div>
+				<MainPromoContent slideData={currentSlide} />
 
 				<div className={styles.photo}>
-					<img src={imageSrc} alt='' className={styles.photoImage} />
+					{/* <img src={imageSrc} alt='' className={styles.photoImage} /> */}
 				</div>
 			</div>
 
--- src/components/MainPromo/components/MainPromoContent/MainPromoContent.jsx
+++ src/components/MainPromo/components/MainPromoContent/MainPromoContent.jsx
@@ -1,18 +1,63 @@
 import React from 'react'
-
+import { useSelector } from 'react-redux'
+import ReactMarkdown from 'react-markdown'
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
 import styles from './MainPromoContent.module.scss'
 
+const transformText = text => {
+	const components = {
+		// eslint-disable-next-line no-unused-vars
+		em: ({ node, ...props }) => <span className={styles.green} {...props} />,
+
+		// eslint-disable-next-line no-unused-vars
+		strong: ({ node, ...props }) => (
+			<span className={styles.greenBig} {...props} />
+		),
+		p: React.Fragment
+	}
+
+	return <ReactMarkdown components={components}>{text}</ReactMarkdown>
+}
+
+const getDataFromCurrentSlide = (currentSlide, currentLanguage) => {
+	return {
+		promoText: transformText(currentSlide.promoText[currentLanguage]),
+		promoPostText: transformText(currentSlide.promoPostText[currentLanguage]),
+		buttonText: transformText(currentSlide.buttonText[currentLanguage]),
+		buttonPostText: transformText(currentSlide.buttonPostText[currentLanguage]),
+		url: currentSlide.url
+	}
+}
+
 const MainPromoContent = ({ slideData }) => {
+	const currentLanguage = useSelector(store => store.language.language)
+
+	const {
+		promoText: contentTitleText,
+		promoPostText: contentPostTitleText,
+		buttonText,
+		buttonPostText: postButtonText,
+		url
+	} = getDataFromCurrentSlide(slideData, currentLanguage)
+
 	return (
 		<div className={styles.content}>
-			<h2 className={styles.contentTitle}>{contentTitleText}</h2>
-			<div className={styles.contentPostTitle}>{contentPostTitleText}</div>
-			<Link to={url} className={styles.button}>
-				{buttonText}
-			</Link>
-			<div className={styles.postButton}>{postButtonText}</div>
+			<h2 className={styles.title}>{contentTitleText}</h2>
+			<div className={styles.postTitle}>{contentPostTitleText}</div>
+
+			<div className={styles.buttonWrapper}>
+				<Link to={url} className={styles.button}>
+					{buttonText}
+				</Link>
+				<div className={styles.postButton}>{postButtonText}</div>
+			</div>
 		</div>
 	)
+}
+
+MainPromoContent.propTypes = {
+	slideData: PropTypes.object
 }
 
 export default MainPromoContent
