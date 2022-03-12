a45b3837e4f13b583275766a2d3054d7a009bd38
Ivan Baluta <ltlaitoff@gmail.com>
2022-03-13 00:25:51 +0200

    Fix redux errors in storybook

--- src/components/Menu/Menu.stories.jsx
+++ src/components/Menu/Menu.stories.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import Menu from './Menu'
+
+import rootReducer from 'store/resucers'
+
+const store = createStore(rootReducer)
 
 export default {
 	title: 'Menu',
 	component: Menu
 }
 const Template = args => (
-	<BrowserRouter>
-		<Menu {...args} />
-	</BrowserRouter>
+	<Provider store={store}>
+		<BrowserRouter>
+			<Menu {...args} />
+		</BrowserRouter>
+	</Provider>
 )
 
 export const Standart = Template.bind({})
--- src/components/MenuAddition/MenuAddition.stories.jsx
+++ src/components/MenuAddition/MenuAddition.stories.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
+
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import MenuAddition from './MenuAddition'
+
+import rootReducer from 'store/resucers'
+
+const store = createStore(rootReducer)
 
 export default {
 	title: 'MenuAddition',
 	component: MenuAddition
 }
 const Template = args => (
-	<BrowserRouter>
-		<MenuAddition {...args} />
-	</BrowserRouter>
+	<Provider store={store}>
+		<BrowserRouter>
+			<MenuAddition {...args} />
+		</BrowserRouter>
+	</Provider>
 )
 
 export const Normal = Template.bind({})
